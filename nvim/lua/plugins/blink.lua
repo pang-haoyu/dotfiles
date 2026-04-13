c97a105952c15bb809e1d9e9cc3bee9cb62c746f
Haoyu Pang <panghaoyu50@gmail.com>
2026-04-13 16:49:08 +1200

    add cpp to nvim

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
     "saghen/blink.cmp",
     opts = {
       sources = {
-        default = { "lsp", "path" },
+        default = { "lsp", "path", "buffer" },
         providers = {
           snippets = {
             enabled = false,
